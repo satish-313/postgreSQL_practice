e20638cac753a61a5c29a0e0a5b2e4f5b6ce2b75
satish-313 <pradhansatish53@gmail.com>
2020-07-11 16:25:09 +0530

    passenger table added

--- table.sql
+++ table.sql
@@ -41,3 +41,59 @@ SELECT * FROM flights WHERE destination LIKE '%o%';
 
 UPDATE flights SET duration = 370 WHERE id = 2;
 
+/* for limit the data from showing */
+SELECT * FROM flights LIMIT 3;
+
+/* for oder the list by accending order of duration */
+SELECT * FROM flights ORDER BY duration ASC;
+
+/* for oder the list by desending order of duration */
+SELECT * FROM flights ORDER BY duration DESC;
+
+/* grouping of data by some counts */
+SELECT origin , COUNT (*) FROM flights GROUP BY origin;
+
+/* grouping by the certain condition of count */
+SELECT origin , COUNT (*) FROM flights GROUP BY origin HAVING COUNT(*) > 1;
+
+/* for creating another table for reference to another or flights table */
+CREATE TABLE passenger ( id SERIAL PRIMARY KEY , name VARCHAR NOT NULL, flights_id INTEGER REFERENCES flights);
+
+/* creating the table for passenger */
+INSERT INTO passenger (name , flights_id) VALUES ('alice',1);
+INSERT INTO passenger (name , flights_id) VALUES ('hari',5);
+INSERT INTO passenger (name , flights_id) VALUES ('peter',2);
+INSERT INTO passenger (name , flights_id) VALUES ('mark',4);
+INSERT INTO passenger (name , flights_id) VALUES ('tony',6);
+INSERT INTO passenger (name , flights_id) VALUES ('luffy',3);
+INSERT INTO passenger (name , flights_id) VALUES ('nami',7);
+
+/* for select name of above and get it id number */
+SELECT * FROM passenger WHERE name = 'tony' ;
+
+/* for geting id in flights table */
+SELECT * FROM flights WHERE id = 6;
+
+/* JOIN USE OF TWO TABLE */
+SELECT origin,destination , name FROM flights JOIN passenger ON passenger.flights_id = flights.id;
+
+/* for particular name as above is tony */
+SELECT origin,destination , name FROM flights JOIN passenger ON passenger.flights_id = flights.id WHERE name = 'tony';
+
+/* for condition is not validate in join table in that case we can use LEFT JOIN METHOD or RIGHT JOIN METHOD */
+SELECT origin,destination , name FROM flights LEFT JOIN passenger ON passenger.flights_id = flights.id;
+
+/* INDEX */
+
+
+/* nested sql*/
+/* more insert in passenger table */
+INSERT INTO passenger (name , flights_id) VALUES ('arvi',1);
+INSERT INTO passenger (name , flights_id) VALUES ('chandra',1);
+INSERT INTO passenger (name , flights_id) VALUES ('kumar',5);
+INSERT INTO passenger (name , flights_id) VALUES ('satish',4);
+INSERT INTO passenger (name , flights_id) VALUES ('pradhan',6);
+
+/* nested loop in sql */
+SELECT * FROM flights WHERE id IN (SELECT flights_id FROM passenger GROUP BY flights_id HAVING COUNT (*) > 1);
+
